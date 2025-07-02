de6e0ddc8b211306357dda41e6cb1cc114736300
roneldornas <roneldornas@hotmail.com>
2025-07-02 23:57:59 +0000

    basic of controller and module

---
+++ src/app.controller.ts
@@ -0,0 +1,15 @@
+import { Controller, Get } from "@nestjs/common";
+
+@Controller('/app')
+export class AppController {
+
+    @Get('/hi')
+    getRootRoute() {
+        return 'Hi there!!';
+    }
+
+    @Get('/bye')
+    getByeThere() {
+        return 'Bye bye!!';
+    }
+}
